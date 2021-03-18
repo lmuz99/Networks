99b6897f6a7f1d6ad3b236f7a03a0ed2df21994a
Luke <lwmuz1@gmail.com>
2021-03-18 13:42:22 +0000

    working er

--- Generator.py
+++ Generator.py
@@ -4,46 +4,88 @@ import random
 from numba import jit
 from numba import cuda
 from numba import prange
+from numba.typed import List
 import networkx as nx
 import timeit
+import cProfile
 
+@jit(nopython = True)
 def GenerateInitial(nodes, m):
     #Method to generate initial network before BA attachment with nodes and m edges per node
-    edge_list = np.array(np.empty(0))
-    sampling_list = np.empty(0)
-    random_vals = np.arange(nodes)
     n = 0       #number of nodes in network
     nubs = 0    #keep track of number of entries of vert_list without calling len function
+    edges = nodes * m
 
-    for i in nodes:
-        for j in m:
-            #choose n*m random pairs from the N(N-1)/2 pairs
-            #check we haven't chose pairs twice
-            #work out which pairs these correspond to
-            #fill in edge list
-            #leave opportunity to break here for analysis
-            pass
+    edge_list = randomEdges(edges, nodes-1)
+    adj_list = edgeToAdjacency(edge_list, nodes)
+    sampling_list = [len(x) for x in adj_list]
+    #convert to adj list
+    #get n and nubs
+    #get sample list as len of each element of edge list
 
-    return edge_list, sampling_list, n, nubs
+
+    return adj_list, sampling_list, n, nubs
+
 
 @jit(nopython = True)
 def randomSample(max_index):
+    #randomly sample a new edge between a pair of nodes, repeating if we get a self loop
+
     x1 = random.randint(0, max_index)
     x2 = random.randint(0, max_index)
-    if x1 == x2:
+    while x1 == x2:
         x2 = random.randint(0, max_index)
         
     return (x1, x2)
 
+
 @jit(nopython = True, parallel = True)
 def randomEdges(n_edges, max_index):
-    output = np.zeros(shape=(n_edges,2), dtype= np.int64)
+    #Creates an edge list as this parallelises random number generation
+
+    edge_list = np.zeros(shape=(n_edges,2), dtype= np.int32)
     for i in prange(n_edges):
-        output[i] = randomSample(max_index)
+        edge_list[i] = randomSample(max_index)
 
-    return output
+    return edge_list
 
+@jit(nopython = True)
+def edgeToAdjacency(edge_list, nodes):
+    
+    #this will do for now, but can potentially speed up by producing a density matrix
+    #by creating a numpy 2d array and filling values, more memory intensive but
+    #no costly creation of empty lists
+
+    #alternatively find a good sorting algo and then we can create arrays as we go rather than needing to initalise empty ones from the start
+
+    adj_list = [[np.int32(x) for x in range(0)] for i in range(nodes)]
+
+    #could split this up/ sort to allow for multithreading down the line
+    for a in edge_list:
+        #only need check for one of the nodes as always write both lists together
+        if isEdgePresent(a[1], a[0]):
+            #get new pair and check this is unique
+            is_pair_unique = False
+            while not is_pair_unique:
+                #generate new pair
+                #if not filled then 
+                pass
+        else:
+            adj_list[a[0]].append(a[1])
+            adj_list[a[1]].append(a[0])
+
+
+    return adj_list
+
+@jit(nopython = True, parallel = True)
+def isEdgePresent(val, list):
+    for i in prange(len(list)):
+        if list[i] == val:
+            return True
+        else:
+            return False
         
+
 
 def BA(n_total, m, n_initial):
     
